dd1b52c14228503daca881ec4b1a8e3f6cca2c58
Jonas <jonas.serpa_stormgroup@prestador.globo>
2022-12-09 17:21:32 -0300

    Remove node_modules

--- dist/configs.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIGS = void 0;
-exports.CONFIGS = {
-    APP_NAME: 'API GATEWAY',
-    PORT: process.env.PORT || 3000
-};
